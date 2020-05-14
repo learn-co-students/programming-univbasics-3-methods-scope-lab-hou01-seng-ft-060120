23b161c0c42a947f88ebaf81f3325b0abe534076
Joe Gomez <jcggomez@live.com>
2020-05-14 05:11:46 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
-def mario
+def mario(phrase = "It's-a me, Mario!")
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
-def toadstool
+def toadstool(status = "Thank You Mario! But Our Princess Is In Another Castle!")
   puts status
+end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  puts mario, toadstool, link
 end
